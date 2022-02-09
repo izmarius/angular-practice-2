4abfc707aa53445bd0f33dd3028c43d1c401aa21
ionesm <marius.ionescu-zinca@msg.group>
2022-02-09 12:09:20 +0100

    components practice

--- practice-app/src/app/app.module.ts
+++ practice-app/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    AlertsComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+   ],
   imports: [
     BrowserModule
   ],
